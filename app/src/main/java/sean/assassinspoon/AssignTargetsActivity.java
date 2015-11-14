ceb93f7d43451f0510726656e7e2979b2edb7ae4
Andy <shena@bu.edu>
2015-11-14 15:51:37 -0500

    minor changes

---
+++ app/src/main/java/sean/assassinspoon/AssignTargetsActivity.java
@@ -0,0 +1,18 @@
+package sean.assassinspoon;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Andy Shen on 11/14/2015.
+ */
+public class AssignTargetsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.assign_targets);
+
+
+    }
+}
