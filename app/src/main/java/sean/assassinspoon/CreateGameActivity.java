5007c0756a22e7704d215c9de7c9a15ca2d9014a
Huy Le <huyle333@bu.edu>
2015-11-14 02:51:44 -0500

    [HL] Updated create activity to have three text boxed with the parameters that we want to add to game. I haven't connected Parse yet, but for now, the button receives the text box names correctly.

--- app/src/main/java/sean/assassinspoon/CreateGameActivity.java
+++ app/src/main/java/sean/assassinspoon/CreateGameActivity.java
@@ -2,15 +2,38 @@ package sean.assassinspoon;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by huyle on 11/14/2015.
  */
 public class CreateGameActivity extends Activity {
 
+    EditText nameOfGame;
+    EditText mapBoundries;
+    EditText numOfPlayers;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_game);
+
+        Button createGame = (Button) findViewById(R.id.createGameButton);
+        nameOfGame = (EditText) findViewById(R.id.nameOfGame);
+        mapBoundries = (EditText) findViewById(R.id.mapBoundries);
+        numOfPlayers = (EditText) findViewById(R.id.numOfPlayers);
+
+        createGame.setOnClickListener(
+            new View.OnClickListener() {
+                public void onClick(View view) {
+                    Log.v("Name of game", nameOfGame.getText().toString());
+                    Log.v("Map boundries", mapBoundries.getText().toString());
+                    Log.v("Number of players", numOfPlayers.getText().toString());
+                }
+            }
+        );
     }
 }
