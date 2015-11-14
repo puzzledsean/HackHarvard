b44514211d17c004fe7220894d7b4c0669a7aa34
Sean Zhang <puzzledsean@gmail.com>
2015-11-14 16:30:02 -0500

    added functionality of player joining games based on names, not object ids

--- app/src/main/java/sean/assassinspoon/joinGameActivity.java
+++ app/src/main/java/sean/assassinspoon/joinGameActivity.java
@@ -2,20 +2,22 @@ package sean.assassinspoon;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.parse.GetCallback;
-import com.parse.Parse;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
 import org.json.JSONArray;
 
-import java.text.ParseException;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Sean on 11/14/15.
@@ -24,6 +26,7 @@ public class joinGameActivity extends AppCompatActivity{
     private EditText editText;
     private String name;
     private String game;
+    private boolean joinedGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,33 +43,60 @@ public class joinGameActivity extends AppCompatActivity{
             public void onClick(View v) {
                 game = gameField.getText().toString();
                 name = nameField.getText().toString();
-                checkNameField(game, name);
-
-                Intent intent = new Intent(joinGameActivity.this, MapActivity.class);
-                startActivity(intent);
+                try {
+                    checkFields(game, name);
+                } catch (com.parse.ParseException e) {
+                    e.printStackTrace();
+                }
+                if(!joinedGame){
+                    InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                    Snackbar.make(v, "A game with that name doesn't exist", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+                else {
+                    Intent intent = new Intent(joinGameActivity.this, MapActivity.class);
+                    startActivity(intent);
+                }
 
             }
         });
 
     }
 
-    private void checkNameField(final String gameName, final String name){
+    private void checkFields(final String gameName, final String name) throws com.parse.ParseException {
 
         ParseQuery<ParseObject> query = ParseQuery.getQuery("Game");
 
-        // Retrieve the object by id
-        query.getInBackground("i4TXC7XjKx", new GetCallback<ParseObject>() {
-            @Override
-            public void done(ParseObject game, com.parse.ParseException e) {
-                game.put("name", gameName);
-                JSONArray temp = game.getJSONArray("listOfPlayers");
+        query.selectKeys(Arrays.asList("name"));
+        List<ParseObject> results = query.find();
+        String gameNameCheck;
+        String objectId;
+        //see if game of that gameName has already been created
+        for(int i = 0; i < results.size(); i++) {
+            gameNameCheck = results.get(i).get("name").toString();
+            Log.d("game name check", gameNameCheck);
+            if(gameNameCheck.equals(gameName)){ //found objectID based on game name
+                joinedGame = true;
+                ParseObject gameObject = results.get(i);
+                objectId = gameObject.fetchIfNeeded().getObjectId();
+                Log.d("objectID", objectId);
+
+                // store the data to that object ID
+                gameObject.put("name", gameName);
+                JSONArray temp = gameObject.fetchIfNeeded().getJSONArray("listOfPlayers");
+                Log.d("temp", temp.toString());
+                Log.d("player name", name);
                 temp.put(name);
-                game.put("listOfPlayers", temp);
+                gameObject.put("listOfPlayers", temp);
                 Log.v("temp ", String.valueOf(temp));
-                game.increment("numOfPlayers");
-                game.saveInBackground();
+                gameObject.increment("numOfPlayers");
+                gameObject.saveInBackground();
+
 
             }
-        });
+        }
+
+
     }
 }
