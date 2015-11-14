1ad45477c2fca901fdbc395ad03b69ed39f3d9a5
Sean Zhang <puzzledsean@gmail.com>
2015-11-14 02:50:37 -0500

    working prototype of join, updates parse database

--- app/src/main/java/sean/assassinspoon/MainActivity.java
+++ app/src/main/java/sean/assassinspoon/MainActivity.java
@@ -1,9 +1,12 @@
 package sean.assassinspoon;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -41,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,6 +73,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 new HeartRateConsentTask().execute(reference);
+            }
+        });
+
+        FloatingActionButton add = (FloatingActionButton) findViewById(R.id.joinBtn);
+        add.setImageDrawable(getDrawable(R.mipmap.ic_launcher));
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, joinGameActivity.class);
+                startActivity(intent);
+
             }
         });
     }
--- app/src/main/java/sean/assassinspoon/joinGameActivity.java
+++ app/src/main/java/sean/assassinspoon/joinGameActivity.java
@@ -2,22 +2,57 @@ package sean.assassinspoon;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.parse.GetCallback;
+import com.parse.Parse;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.text.ParseException;
 
 /**
  * Created by Sean on 11/14/15.
  */
 public class joinGameActivity extends AppCompatActivity{
+    private EditText editText;
+    private String name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.join_game_activity);
 
-        ParseObject game = new ParseObject("Game");
-//        gameScore.put("score", 1337);
-//        gameScore.put("playerName", "Sean Plott");
-//        gameScore.put("cheatMode", false);
-//        gameScore.saveInBackground();
+
+        editText = (EditText) findViewById(R.id.nameField);
+        Button startButton = (Button) findViewById(R.id.start_after_join);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name = editText.getText().toString();
+                checkNameField(name);
+
+            }
+        });
+
+    }
+
+    private void checkNameField(final String name){
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Game");
+
+        // Retrieve the object by id
+        query.getInBackground("i4TXC7XjKx", new GetCallback<ParseObject>() {
+            @Override
+            public void done(ParseObject game, com.parse.ParseException e) {
+                Log.d("name", game.getString("name"));
+                game.put("name", name);
+                game.saveInBackground();
+
+            }
+        });
     }
 }
