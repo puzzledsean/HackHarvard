4acbca572ef78abdcf6ec0ce4f9f2b4d81d31834
Andy <shena@bu.edu>
2015-11-14 23:07:55 -0500

    added target xml files

---
+++ app/src/main/java/sean/assassinspoon/TargetInfoPageActivity.java
@@ -0,0 +1,25 @@
+package sean.assassinspoon;
+
+import android.graphics.Typeface;
+import android.media.Image;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Andy Shen on 11/14/2015.
+ */
+public class TargetInfoPageActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.target_information_page);
+
+
+
+    }
+
+
+}
